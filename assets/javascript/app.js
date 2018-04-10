496de01502f63e8ff42e0110adc3a2593c10d507
janell <janell.williams@ticketmaster.com>
2018-04-09 21:30:55 -0700

    research

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,5 +17,42 @@ $(document).ready(function () {});
  var database = firebase.database();
 
 
+ //Ajax test call 
+queryURL = "https://www.googleapis.com/calendar/v3/calendars/primary/events?sendNotifications=true&supportsAttachments=true&fields=endTimeUnspecified%2Cgadget%2Corganizer%2Creminders%2FuseDefault%2Cstart&key=AIzaSyABfyoUKd5LoeP8AXbn05AXwSjr9C4LCcY"
+appointment = {
+    "end": {
+     "dateTime": "2018-05-01T11:00:00",
+     "timeZone": "America/Los_Angeles"
+    },
+    "start": {
+     "dateTime": "2018-05-01T10:00:00",
+     "timeZone": "America/Los_Angeles"
+    },
+    "description": "appointment ",
+    "attendees": [
+     {
+      "email": "jcw_124@sbcglobal.net"
+     }
+    ],
+    "reminders": {
+     "useDefault": true
+    }
+    }
+    
 
+ $.ajax({
+    url: queryURL,
+    headers: {
+        'Content-Type':'application/json',
+        'Access-Control-Allow-Origin' : '*'
+    },
+    dataType: 'json',
+    data: appointment,
+    method: "POST"
+  }).then(function(response) {
+    
+    console.log(response);
 
+    var  results = (response.data) ;
+
+   })
