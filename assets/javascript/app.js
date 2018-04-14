18272edae5a8856ec075deece7f87ada03a92afa
janell <janell.williams@ticketmaster.com>
2018-04-14 12:51:10 -0700

    contact us db link

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {});
 
-
-
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyD9JPRHwIhDlaJtCx2WQC0OxJpFO2n6y4Q",
@@ -16,6 +14,45 @@ $(document).ready(function () {});
   //Initialize database variable
  var database = firebase.database();
 
+ //Contact Us form variables
+ var ContactFirstName, ContactLastName, ContactEmail, ContactPhone, ContactMessage;
 
 
+ // Capture Contact info from form
+$("#signin").on("click", function(event) {
+    event.preventDefault();
+
+console.log("button click function line 21");
+
+//Collect values from contact us form
+ContactFirstName = $("#fname").val().trim();
+ContactLastName = $("#lname").val().trim();
+ContactEmail = $("#emailAdd").val().trim();
+ContactPhone = $("#phone").val().trim();
+ContactMessage = $("#message").val().trim();
+
+console.log(ContactFirstName + " " + ContactLastName);
+console.log(ContactEmail);
+console.log(ContactPhone);
+console.log(ContactMessage);
+
+// Push contact us values to database
+database.ref("contacts").push({
+  FirstName: ContactFirstName,
+  LastName: ContactLastName,
+  Email: ContactEmail,
+  PhoneNumber: ContactPhone,
+  Message: ContactMessage,
+  dateAdded: firebase.database.ServerValue.TIMESTAMP
+});
+});
+// Firebase watcher 
+database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
+  // storing the snapshot.val() in a variable for convenience
+  var contact = snapshot.val();
+
+  // Handle the errors
+}, function(errorObject) {
+  console.log("Errors handled: " + errorObject.code);
+});
 
